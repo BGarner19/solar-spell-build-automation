5d50ff26d9814cb01a6d64539d9d21850214db82
BGarner19 <bailey.garner@asu.edu>
2018-10-30 23:56:39 -0700

    Editing disk space method to not throw an error on Windows systems

--- build_automation/content_management/utils.py
+++ build_automation/content_management/utils.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import os
+import platform
 import shutil
 import tarfile
 import tempfile
@@ -350,16 +351,19 @@ class LibraryVersionBuildUtil:
 
 
 class DiskSpace:
-    
 
     def getfreespace(self):
-		
+
+        print(platform.system())
+
         if platform.system() == 'Windows':
-            disk_stats = os.statvfs('/')
-            block_size = disk_stats.f_frsize
-            avail_blocks = disk_stats.f_bavail
-            total_blocks = disk_stats.f_blocks
-		
-        free_space = self.block_size * self.avail_blocks
-        total_space = self.total_blocks * self.block_size
+            return 0, 0
+
+        disk_stats = os.statvfs('/')
+        block_size = disk_stats.f_frsize
+        avail_blocks = disk_stats.f_bavail
+        total_blocks = disk_stats.f_blocks
+
+        free_space = block_size * avail_blocks
+        total_space = total_blocks * block_size
         return (free_space, total_space)
