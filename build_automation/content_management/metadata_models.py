e86bdf1a9f9c23107cfea66e3888d00ce1fd0c39
Bailey Garner <bailey.garner@asu.edu>
2019-01-19 05:00:39 -0700

    create object to represent new metadata fields

--- build_automation/content_management/metadata_models.py
+++ build_automation/content_management/metadata_models.py
@@ -2,7 +2,8 @@ import os
 
 from django.db import models
 
-class Content(models.Model):
+
+class DB_Content(models.Model):
 
     file_name = models.CharField(max_length=300)
     source = models.TextField()
@@ -20,6 +21,31 @@ class Content(models.Model):
     copyright = models.TextField()
     rights_statement = models.TextField()
     contributors = models.ManyToManyField(Contributor)
+
+
+class ContentMetadata(object):
+
+    def __init__(self, file_name, source, title, creators, date_created, coverage, library_version, main_folder, sub_folder, subject, keywords, workareas, language, copyright, rights_statement, contributors):
+        self.file_name = file_name
+        self.source = source
+        self.title = title
+        self.creators = creators
+        self.date_created = date_created
+        self.coverage = coverage
+        self.library_version = library_version
+        self.main_folder = main_folder
+        self.sub_folder = sub_folder
+        self.subject = subject
+        self.keywords = keywords
+        self.workareas = workareas
+        self.language = language
+        self.copyright = copyright
+        self.rights_statement = rights_statement
+        self.contributors = contributors
+
+
+
+
 
 
 
