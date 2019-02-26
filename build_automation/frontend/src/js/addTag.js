2d170b567713e83fdf634977bb1061cf56c2047f
JJanas <JJanas@asu.edu>
2019-02-25 22:45:04 -0700

    Documented addTag

--- build_automation/frontend/src/js/addTag.js
+++ build_automation/frontend/src/js/addTag.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 import React from 'react';
 
-import Button from 'material-ui/Button';
-import TextField from 'material-ui/TextField';
-import Grid from 'material-ui/Grid';
-import Snackbar from 'material-ui/Snackbar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import { APP_URLS, get_url } from './url.js';
 
-import Typography from 'material-ui/Typography';
+import Typography from '@material-ui/core/Typography';
 import { TAG_SAVE_TYPE, HTTP_STATUS } from './constants.js';
 class TagCreation extends React.Component {
     constructor(props) {
         super(props);
-        
         this.state = {
             id: props.tag.id,
             name: props.tag.name,
@@ -24,7 +23,9 @@ class TagCreation extends React.Component {
         this.saveCallback = props.onSave.bind(this);
         this.handleCloseSnackbar = this.handleCloseSnackbar.bind(this);
     }
-
+    /*
+    * This method handles changes made to fields in metadata files
+    */
     handleTextFieldUpdate(stateProperty, evt) {
         this.setState({
             [stateProperty]: evt.target.value
@@ -35,7 +36,9 @@ class TagCreation extends React.Component {
         return this.state.messageType === "error" ? { backgroundColor: '#B71C1C', fontWeight: 'normal' } : {};
     }
 
-
+    /*
+    * A method to populate the files data
+    */
     componentWillReceiveProps(props) {
 
         this.setState({
@@ -44,13 +47,15 @@ class TagCreation extends React.Component {
 
         });
     }
-
+    /*
+    * Method to either update or create a new piece of metadata
+    */
     saveTag(evt) {
         var targetUrl = this.props.listUrl;
         const payload = { name: this.state.name, description: this.state.description };
         const currentInstance = this;
         if (this.state.id > 0) {
-            // Update an exising Tag.
+            // Update an existing Tag.
             targetUrl = get_url(this.props.detailUrl, { id: this.state.id });
             axios.patch(targetUrl, payload, {
                 responseType: 'json'
@@ -86,7 +91,6 @@ class TagCreation extends React.Component {
                     message: errorMsg,
                     messageType: 'error'
                 });
-                
             })
 
 
@@ -96,20 +100,22 @@ class TagCreation extends React.Component {
 
 
 
-
+    /*
+    * Render class for the add metadata screen
+    */
     render() {
         return (
             <Grid container spacing={0} style={{ paddingLeft: '20px' }}>
                 <Grid item xs={8}>
-                    <Button variant="raised" color="primary" onClick={e => { this.props.onCancel() }}>
+                    <Button variant="contained" color="primary" onClick={e => { this.props.onCancel() }}>
                         Cancel
                     </Button>
-                    <Button variant="raised" color="primary" onClick={evt => this.saveTag(evt)}>
+                    <Button variant="contained" color="primary" onClick={evt => this.saveTag(evt)}>
                         Save
                     </Button>
                 </Grid>
                 <Grid item xs={8}>
-                    <Typography gutterBottom align="center" variant="headline" component="h2">
+                    <Typography gutterBottom align="center" variant="h5" component="h2">
                         {this.props.title}
                     </Typography>
                 </Grid>
@@ -140,7 +146,7 @@ class TagCreation extends React.Component {
                     open={Boolean(this.state.message)}
                     onClose={this.handleCloseSnackbar}
                     message={<span>{this.state.message}</span>}
-                    SnackbarContentProps={{
+                    ContentProps={{
                         "style": this.getErrorClass()
                     }}
                 />
@@ -151,6 +157,9 @@ class TagCreation extends React.Component {
 
 
     }
+    /*
+    * Close snackbar
+    */
     handleCloseSnackbar() {
         this.setState({
             message: null,
@@ -159,4 +168,4 @@ class TagCreation extends React.Component {
     }
 }
 
-module.exports = TagCreation;
+export default TagCreation;
